24f10b6e6eb81180e8115f0158ebcef88e1ed24a
agnoam <agnoam.aghai.ooo.123@gmail.com>
2018-09-23 04:16:39 +0300

    added new api keys and function to get more

--- app/gb.js
+++ app/gb.js
@@ -1,3 +1,50 @@
+function hack(numOfKeys) {
+    var apiKeysArr = JSON.parse(localStorage.getItem('apiArr'));
+        
+    if(!apiKeysArr) {
+        apiKeysArr = [];
+    }
+
+    // Check if the arr length < 10
+    if(apiKeysArr.length < numOfKeys) {
+        var form = document.getElementById('post-form');
+        form.first.value = 'a';
+        form.first.value = "a"
+        form.last.value = "a"
+        form.occupation.value = "Investor"
+        form.organization.value = `d${Date.now()-327863728166}ff`
+        form.email.value = `${Date.now()}@gmail.com`
+    //     var rep = document.getElementsByClassName('g-recaptcha')[0];
+    //     rep.parentElement.removeChild(rep);
+        document.getElementById('submit-btn').disabled = false;
+        document.getElementById('submit-btn').click();
+
+        // Wait for 2sec, for the page will render the apiKey
+        setTimeout(() => {
+            var apiCodeTag = document.getElementsByTagName('strong')[1];
+            var apiCodeString = apiCodeTag.textContent;
+
+            // exporting the api code from the gb text
+            apiCodeString = apiCodeString.split(':')[1];
+            apiCodeString = apiCodeString.split('.')[0];
+            var apiCode = apiCodeString.split(' ')[1];
+
+            // Checking if this apiKey is already imported to the array
+            if(!apiKeysArr.includes(apiCode)) {
+                apiKeysArr.push(apiCode);
+                localStorage.setItem('apiArr', JSON.stringify(apiKeysArr));
+            }
+
+			hack(numOfKeys);
+        }, 2000);
+    } else {
+		console.log(apiKeysArr);
+		console.log('done. please check the array');
+    }
+}
+
+// document.getElementsByTagName('iframe')[0].scrollIntoView()
+// setTimeout(() => { document.getElementById('submit-btn').click() }, 5000)
 const VERS1 = 'Vantage! Your API key is: ';
 const VERS2 = 'Vantage! Your dedicated access key is: ';
 const VERS3 = 'It seems that you are already a registered user. As a reminder, your API key is: ';
@@ -65,4 +112,43 @@ generateNewApiKey()
 // const fullDate = `${year}-${month2Dig}-${date2Dig}`;
 // console.log(fullDate);
 
+                // console.log(`Success - ${tickerSymbol} - ${stock_al['Time Series (Daily)'][quoteList[0]]['1. open']}`);
+
+
+
+// const https = require("https");
+// const apikey: string = ApiKey[0];
+
+
+// type my = {
+//     s?: {
+//         df?: {
+//             age?: number 
+//         }
+//     }
+// }
+
+// var b: my = {
+
+// }
+// var c: my = {
+//     s: {
+//         df: {}
+//     }
+// }
+// var a: my = {
+//     s: {
+//         df: {
+//             age: 44
+//         }
+//     }
+// }
+// if (a && a.s && a.s.df && a.s.df.age) {
+//     console.log (a.s.df.age)
+// }
+
+// console.log(a && a.s && a.s.df && a.s.df.age)
+
+// var val = _.get(a, 's.df.age', 0);
+// console.log(val)
 // console.log(`Success - ${tickerSymbol} - ${stock_al['Time Series (Daily)'][quoteList[0]]['1. open']}`);
