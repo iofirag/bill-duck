f01548acafa5d6cfe3cfb7dbd2dab5731545fe29
agnoam <agnoam.aghai.ooo.123@gmail.com>
2018-09-25 06:31:59 +0300

    added connection and return db object from mongoClient

--- app/config/mongo.config.ts
+++ app/config/mongo.config.ts
@@ -1,18 +1,23 @@
-import { MongoClient, Server } from "mongodb";
+import { MongoClient, Server, Db } from "mongodb";
 
 export class MongoConfig {
     mongoURL: string = '';
     port: number = 8858;
+    dbObject: Db | undefined = undefined;
     
     constructor(isLocal?: boolean) {
         if(isLocal) {
-            new Server('localhost', this.port);
+            MongoClient.connect(`localhost:${this.port}`, (err, db) => {
+                err ? console.error('mongoDB ex:', err) : console.log('connected successfuly to local mongoDB');
+
+                this.dbObject = db.db();
+            });
         } else {
             // Connect using MongoClient
             MongoClient.connect(this.mongoURL, (err, db) => {
-                err ? console.error('mongoDB ex:', err) : null;
+                err ? console.error('mongoDB ex:', err) : console.log('connected successfuly to online mongoDB');
                 
-                return db;
+                this.dbObject = db.db();
             });
         }
     }
